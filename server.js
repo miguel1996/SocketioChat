06021d4594a1e9aa0f35edf06a71eedd5cf6d2ea
toomaas <toomaas8@hotmail.com>
2019-01-05 12:15:56 +0000

    showing users on client

--- server.js
+++ server.js
@@ -11,19 +11,31 @@ console.log('Server is running');
 
 const users = [];
 const connections = [];
-
 io.sockets.on('connection',(socket) => {
    connections.push(socket);
+   users.push(socket.id);
    console.log(' %s sockets is connected', connections.length);
+   socket.emit('start',{message:socket.id});
+
+   io.sockets.emit('all sockets',{message: users});
 
    socket.on('disconnect', () => {
+      console.log("disconect "+socket.id);
+      users.splice(users.indexOf(socket.id), 1);
       connections.splice(connections.indexOf(socket), 1);
    });
 
    socket.on('sending message', (message) => {
       console.log('Message is received :', message);
+      io.sockets.emit('new message', {message: message,socket:socket.id});
 
-      io.sockets.emit('new message', {message: message});
+   });
+
+   socket.on('private',(data) =>{
+      if(data.target=123)
+      socket.emit('123',info);
+      if(data.target=321)
+      socket.emit('321',info);
    });
 });
 
